d6025bc7adf742631c0db3a77dc5d451a5c604e5
james <james20209@msn.com>
2017-11-14 14:43:10 +0800

    x

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-	 var urlData1 = "../data/data1.json";
-	 var urlData2 = "../data/data2.json";
-	 var urlData3 = "../data/data3.json";
-	$.getJSON("/urlData1", function(data){ 
+	 var urlData1 = "data/data1.json";
+	 var urlData2 = "data/data2.json";
+	 var urlData3 = "data/data3.json";
+	$.getJSON("urlData1", function(data){ 
 		console.log(data);
 	});
 });
