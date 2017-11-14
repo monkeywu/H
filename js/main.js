5da56ae9f4da95570c3ca3d8ac6bab218c83693a
james <james20209@msn.com>
2017-11-14 14:27:07 +0800

    x

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
-	var urlData1 = "data/data1.json";
-	var urlData2 = "data/data2.json";
-	var urlData3 = "data/data3.json";
-	var xhr = new XMLHttpRequest();
-	xhr.open('get','../urlData1',false);
-	xhr.send('null');
-	var data = JSON.parse(xhr.responseText);
-	console.log(data);
+	 var urlData1 = "../data/data1.json";
+	 var urlData2 = "../data/data2.json";
+	 var urlData3 = "../data/data3.json";
+	 $.get('urlData1',function(data){
+	  console.log(data);
+	 })
 });
