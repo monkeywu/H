9784baa0800a504b9a5f6191412700aada42c95b
james <james20209@msn.com>
2017-11-14 17:07:57 +0800

    2

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 		var length = data.length;
 		console.log(length);
 		for (let i =0;i<length;i++){
-				$('tr').after('<tr></tr>')
+				console.log(i);
 			for (let key in data[i]) {
 			    //$('tr').last().append(data[i][key]);
 			    //$('tr').after('<td>'+data[i][key]+'</td>');
