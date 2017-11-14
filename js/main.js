333092ceb33aa2e1c374aebcd2a5517b5c794e86
james <james20209@msn.com>
2017-11-14 14:25:48 +0800

    x

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	var urlData2 = "data/data2.json";
 	var urlData3 = "data/data3.json";
 	var xhr = new XMLHttpRequest();
-	xhr.open('get','urlData1',false);
+	xhr.open('get','../urlData1',false);
 	xhr.send('null');
 	var data = JSON.parse(xhr.responseText);
 	console.log(data);
