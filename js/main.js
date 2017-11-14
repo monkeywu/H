723d82b5318d2674f08a5a15db802db2f817526b
james <james20209@msn.com>
2017-11-14 13:54:42 +0800

    x

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	var urlData1 = "data/data1.json";
+	var urlData2 = "data/data2.json";
+	var urlData3 = "data/data3.json";
+	$.get('urlData1',function(data){
+		console.log(data);
+	})
+});
