b0b6336eac5ccb8508f1bbd062d5dd43b904b56d
james <james20209@msn.com>
2017-11-14 14:50:51 +0800

    z

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	 var urlData1 = "../data/data1.json";
+	 var urlData1 = "data/data1.json";
 	 var urlData2 = "data/data2.json";
 	 var urlData3 = "data/data3.json";
 	$.get(urlData1,function(data){
