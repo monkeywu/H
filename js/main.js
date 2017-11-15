124d6eba3bd04cf30ac4a3e381dc262a22fb8849
james <james20209@msn.com>
2017-11-15 15:37:10 +0800

    123

--- js/main.js
+++ js/main.js
@@ -28,7 +28,11 @@ function checkData3(){
 function addData(){
 	$('table').append('<tbody></tbody>');
 	for(let i =0;i<Data1.length;i++){
-		$('tbody').append('<tr><td><span class="star">'+Data1[0]+'</span></td><td>'+Data1[1]+'</td><td>'+Data1[2]+'</td><td>'+Data1[3]+'</td><td>'+Data1[4]+'</td><td>'+Data1[5]+'</td><td>'+Data1[6]+'</td><td>'+Data1[7]+'</td><td>'+Data1[8]+'</td><td>'+Data1[9]+'</td></tr>');
+		$('tbody').append('<tr><td><span class="star">'+Data1[i][0]+'</span></td></tr>');
+		for(let j = 0;j<9;j++){
+			$('tbody tr:last-child').append()
+			append('<tr>'+Data1[j][1]+'</td><td>'+Data1[j][2]+'</td><td>'+Data1[j][3]+'</td><td>'+Data1[j][4]+'</td><td>'+Data1[j][5]+'</td><td>'+Data1[j][6]+'</td><td>'+Data1[j][7]+'</td><td>'+Data1[j][8]+'</td><td>'+Data1[j][9]+'</td></tr>');
+		}
 	}
 }
 
