b896baa223609f94e6b1d696acd32f6848088a18
james <james20209@msn.com>
2017-11-15 13:20:11 +0800

    q

--- js/main.js
+++ js/main.js
@@ -3,9 +3,16 @@ $(document).ready(function(){
 	 var urlData1 = "data/data1.json";
 	 var urlData2 = "data/data2.json";
 	 var urlData3 = "data/data3.json";
-	$.get(urlData1,function(data){
 
+	 var Data1;
+	 var Data2;
+	 var Data3;
+
+
+	$.get(urlData1,function(data){
 		var length = data.length;
+		Data1 += data;
+		console.log(Data1);
 		$('table').append('<tbody></tbody>');
 		for (let i =0;i<length;i++){
 				$('tbody').append('<tr></tr>');
