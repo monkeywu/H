4fd4cb6829b0ca17e8a86b8e45718f33d427c67e
james <james20209@msn.com>
2017-11-16 14:42:59 +0800

    q

--- js/main.js
+++ js/main.js
@@ -92,6 +92,7 @@ $(document).ready(function(){
 
 $(document).on('click','tr',function(){
 	$(this).toggleClass('selected');
+	$(this).siblings().removeClass('selected');
 })
 
 $(document).on('click','.star',function(event){
