42f9f0d53e880e2869f816be0d1cf3061ccfef79
james <james20209@msn.com>
2017-11-20 20:23:17 +0800

    q

--- js/main.js
+++ js/main.js
@@ -3,23 +3,33 @@ var Data2 = [];
 var Data3 = [];
 var arr1 = [];
 var arr2 = [];
+
+//網頁讀取開始時間
 var startTime = new Date().getTime();
 
+//將Data2內資料整理
 function checkData2(){
 
 	Data2 = Data2.sort(function (a, b) {
 	    return a.key.slice(1) - b.key.slice(1)
 	});
-
 }
 
+//將Data3內資料整理
 function checkData3(){
 
 	Data3 = Data3.sort(function (a, b) {
 	    return a.cell4.match(/\d+/g)[0] - b.cell4.match(/\d+/g)[0];
 	});
+
+	}
+//讀取結束後將秒數輸出至html上
+	var endTime = new Date().getTime();
+	var costTime = Math.floor(endTime - startTime )
+	$('.usuage').html(costTime);
 }
 
+//將比對完的data1輸出至html上
 function addData(){
 	$('table').append('<tbody></tbody>');
 	for(let i =0;i<Data1.length;i++){
@@ -31,21 +41,21 @@ $(document).ready(function(){
 	 var urlData1 = "data/data1.json";
 	 var urlData2 = "data/data2.json";
 	 var urlData3 = "data/data3.json";
-
+	 //從data1讀取json，每八筆資料組成一個小陣列，再丟到Data1以供之後輸出
 	ajax1 = $.get(urlData1,function(data){
 		var length = data.length;
 		for (let i =0;i<length;i++){
 				Data1.push(data[i]);
 		}
 	})
-
+	//從data2讀取json，再丟到Data2以供之後輸出
 	ajax2 = $.get(urlData2,function(data){
 			var length = data.length;
 			for (let i =0;i<length;i++){
 					Data2.push(data[i]);
 			}
 		})	
-
+	//從data3讀取json，再把資料丟到Data3以供之後輸出
 	ajax3 = $.get(urlData3,function(data){
 				var objkey = Object.values(data);
 				var length = Object.values(data).length;
@@ -53,7 +63,7 @@ $(document).ready(function(){
 					Data3.push(objkey[i]);
 				}
 			})
-
+	//當三筆ajax都讀取完後執行function
 	　$.when(ajax1, ajax2, ajax3).done(function(){
 		var endTime = new Date().getTime();
 		var costTime = Math.floor(endTime - startTime );
@@ -69,9 +79,10 @@ $(document).ready(function(){
 
 });
 
+//點擊增加selected class
+
 $(document).on('click','tr',function(){
 	$(this).toggleClass('selected');
-	$(this).siblings().removeClass('selected');
 })
 
 $(document).on('click','.star',function(event){
